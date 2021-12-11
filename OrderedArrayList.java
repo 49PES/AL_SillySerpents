2721fd4869bcbf4b08a6756fd20d82d193a140a2
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2021-12-11 16:39:02 -0500

    Update OrderedArrayList.java

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -23,5 +23,20 @@ public class OrderedArrayList {
   public int size() {
     return _data.size();
   }
+  
+  // Adds an element to the list, ensuring that ascending order is preserved
+  void add(int num){
+        int index = -1;
+        for(int i = 0 ; i < _data.size(); i++ ){
+            if(_data.get(i) >= num){
+                _data.add(i, num);
+                index = i;
+                i = _data.size(); // This line allows the for-loop to be broken - is there any cleaner way of accomplishing this?
+            }
+        }
+        
+        if (index == -1) _data.add(num); // If the value is greater than all the values in the list, then add it to the end
+    
+    }
 
 }
